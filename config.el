e365fecc4f5937e4f11c80de0caf6b7362657467
raulmpad <raul@deceroainfinito.com>
2019-07-28 17:52:21 +0200

    org-super-agenda package added

--- config.el
+++ config.el
@@ -1,3 +1,25 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+;; (setq doom-font (font-spec :family "Source Code" :size 20) doom-big-font (font-spec :family "Source Code" :size 36))
+
+(def-package! org-super-agenda
+  :after org-agenda
+  ;; before the package is loaded
+  :init
+  (setq org-super-agenda-groups '((:name "Today"
+                                         :time-grid t
+                                         :scheduled today)
+                                  (:name "Due today"
+                                         :deadline today)
+                                  (:name "Important"
+                                         :priority "A")
+                                  (:name "Overdue"
+                                         :deadline past)
+                                  (:name "Due soon"
+                                         :deadline future)
+                                  (:name "Big Outcomes"
+                                         :tag "bo")))
+  ;; after the package is loaded
+  :config
+  (org-super-agenda-mode))
