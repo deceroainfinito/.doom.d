e78882fdbed0feb705af7ec3025060112aea31df
deceroainfinito <raul@deceroainfinito.es>
2019-08-02 15:16:48 +0200

    search's results to a new buffer function

--- config.el
+++ config.el
@@ -69,3 +69,19 @@
 
 ;; Maximize on startup
 (set-frame-parameter (selected-frame) 'fullscreen 'fullboth)
+
+(defun copy-lines-matching-re (re)
+  "find all lines matching the regexp RE in the current buffer
+putting the matching lines in a buffer named *matching*"
+  (interactive "sRegexp to match: ")
+  (let ((result-buffer (get-buffer-create "*matching*")))
+    (with-current-buffer result-buffer
+      (erase-buffer))
+    (save-match-data
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward re nil t)
+          (princ (buffer-substring-no-properties (line-beginning-position)
+                                                 (line-beginning-position 2))
+                 result-buffer))))
+    (pop-to-buffer result-buffer)))
