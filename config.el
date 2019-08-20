47ea59dc8cf74d0d3f9a7aa75f50d16c6152cfbb
deceroainfinito <raul@deceroainfinito.es>
2019-08-20 18:39:27 +0200

    removes my-config.el
    Many other minor changes

--- config.el
+++ config.el
@@ -1,6 +1,4 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
- ;; fullscreen at startup
-(set-frame-parameter (selected-frame) 'fullscreen 'fullboth)
  ;; Literate init
 (org-babel-load-file (expand-file-name "~/.doom.d/my-config.org"))
