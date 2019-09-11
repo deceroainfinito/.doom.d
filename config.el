7591664ddea764e5d0f85aa6c432d8573ee2be0b
deceroainfinito <raul@deceroainfinito.es>
2019-09-12 00:12:25 +0200

    add author
    more org-todo-keywords
    org-id
    hunspell
    more org-agenda-files

--- config.el
+++ config.el
@@ -1,3 +1,7 @@
+(setq user-full-name "deceroainfinito"
+      user-mail-address "raul@deceroainfinito.es"
+      epa-file-encrypt-to user-mail-address)
+
 (setq
  +doom-dashboard-banner-file (expand-file-name "red_dice_logo.png" doom-private-dir))
 
@@ -19,7 +23,7 @@
   (doom-themes-org-config)
 
   :config
-  (load-theme 'doom-nord t))
+  (load-theme 'doom-one t))
 
 (map! :ne "M-=" (λ! (text-scale-set 0))
       :ne "M-+" #'text-scale-increase
@@ -37,9 +41,8 @@
 (after! org
   :config
   (setq org-link-frame-setup '((file . find-file-other-window)))
-  (setq org-todo-keywords '((sequence "TODO(t)" "|")
-                            (sequence "[!](i)" "[ ](E)" "[-](g)" "[?](q)" "|" "[X](D)")
-                            (sequence "NEXT(n)" "WAIT(w)" "HOLD(h)" "|" "ABRT(c)")
+  (setq org-todo-keywords '((sequence "[!](i)" "[ ](E)" "[-](g)" "[?](q)" "|" "[X](x)")
+                            (sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "HOLD(h)" "|" "ABRT(c)" "DONE(d)")
                             (sequence "[READY FOR REVIEW](W)"
                                       "[REVIEW FAILED](F)"
                                       "[READY FOR TESTING](T)"
@@ -50,33 +53,45 @@
                                       "[READY FOR PRODUCTION](P)"
                                       "|"
                                       "[MERGED](M)"
-                                      "[DONE](d)"))))
+                                      "[DONE](D)"))))
 
 (map! :map outline-mode-map
       :n "zw" #'widen)
 
+(map! :map org-mode-map
+      :n "gi" #'org-id-copy
+      :n "ci" #'org-id-get-create)
+
 (after! org
   :init
   (add-to-list 'org-modules 'org-habit t)
-  )
+  :config
+  (setq org-agenda-files (list "~/ownbujo" "~/badibujo/"))
+  (setq org-habit-show-all-today t)
+  (unless (string-match-p "\\.gpg" org-agenda-file-regexp)
+    (setq org-agenda-file-regexp
+          (replace-regexp-in-string "\\\\\\.org" "\\\\.org\\\\(\\\\.gpg\\\\)?"
+                                    org-agenda-file-regexp))))
 
 (def-package! org-super-agenda
   :after org-agenda
   ;; before the package is loaded
   :init
-  (setq org-super-agenda-groups '((:name "Today"
-                                         :time-grid t
-                                         :scheduled today)
-                                  (:name "Due today"
-                                         :deadline today)
-                                  (:name "Important"
-                                         :priority "A")
-                                  (:name "Overdue"
-                                         :deadline past)
-                                  (:name "Due soon"
-                                         :deadline future)
-                                  (:name "Big Outcomes"
-                                         :tag "bo")))
+  ;; (setq org-super-agenda-groups '((:name "Today"
+  ;;                                        :time-grid t
+  ;;                                        :scheduled today)
+  ;;                                 (:name "Due today"
+  ;;                                        :deadline today)
+  ;;                                 (:name "Important"
+  ;;                                        :priority "A")
+  ;;                                 (:name "Habits"
+  ;;                                        :property "habit")
+  ;;                                 (:name "Overdue"
+  ;;                                        :deadline past)
+  ;;                                 (:name "Due soon"
+  ;;                                        :deadline future)
+  ;;                                 (:name "Big Outcomes"
+  ;;                                        :tag "bo")))
   ;; after the package is loaded
   :config
   (org-super-agenda-mode))
@@ -113,3 +128,18 @@ putting the matching lines in a buffer named *matching*"
                            (org-match-string-no-properties 1))))
         (apply 'delete-region remove)
         (insert description))))
+
+(defun hunspell/check-english
+            (interactive)
+            (ispell-change-dictionary "en_GB"))
+(defun hunspell/check-spanish
+            (interactive)
+            (ispell-change-dictionary "es_es"))
+
+(after! ispell
+  :config
+  (setq ispell-program-name (executable-find "hunspell")
+      ispell-dictionary "en_GB")
+  ;; (map! :map org-mode-map
+  ;;     :n "he" #'hunspell/check-spanish
+  ;;     :n "hi" #'hunspell/check-english))
